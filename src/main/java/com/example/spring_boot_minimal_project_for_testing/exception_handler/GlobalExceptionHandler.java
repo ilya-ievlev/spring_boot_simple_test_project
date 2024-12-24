c957bf12bf882bd3bfcbf0f4c13c6f18b459115c
ilya <ievlev2972@gmail.com>
2024-12-24 13:14:51 +0200

    some new changes

--- src/main/java/com/example/spring_boot_minimal_project_for_testing/exception_handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/spring_boot_minimal_project_for_testing/exception_handler/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.spring_boot_minimal_project_for_testing;
+package com.example.spring_boot_minimal_project_for_testing.exception_handler;
 
 
 import org.aspectj.lang.annotation.AfterThrowing;
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class GlobalExceptionHandler {
 
-    @AfterThrowing(pointcut = "execution(* com.example.spring_boot_minimal_project_for_testing.SpringBootMinimalProjectForTestingApplication.main(..))", throwing = "ex")
+    @AfterThrowing(pointcut = "execution(* com.example.spring_boot_minimal_project_for_testing.SpringBootMinimalProjectForTestingApplication..*(..))", throwing = "ex")
     public void logException(Exception ex) {
         System.out.println(ex.getMessage()+"from aop");    }
 }
